17d01c96b57a472e15a5071e742ad38f64cd0b0a
Kevin Li <kevinli5506@gmail.com>
2025-12-26 17:35:51 +0700

    Day 2: Python variables, types, and basic output

---
+++ day2_basics.py
@@ -0,0 +1,36 @@
+#  Day 2 - Python Basics
+# 1.Variable
+
+name = "Kevin"
+age = 25
+height = 179.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+print("---")
+# 2.Type checking
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_student:", type(is_student))
+
+print("---")   
+
+# Simple math operations
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+print("---")
+
+# 4.String operations
+print("Hello, my name is", name)
+print(f"My name is {name} and i am {age} years old.")
